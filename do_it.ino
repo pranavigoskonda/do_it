7f8c7f2a880f428bfc351a303e8a3c50bbb2f436
pranavigoskonda <pranavi.goskonda.427@my.csun.edu>
2026-08-27 00:24:35 -0700

    update the program

--- do_it.ino
+++ do_it.ino
@@ -11,12 +11,12 @@ void loop() {
     // Turn the blue LED ON
     digitalWrite(LED_PIN, HIGH);
 
-    // Wait for 5 seconds
-    delay(5000);
+    // Wait for 4 seconds
+    delay(7500);
 
     // Turn the blue LED OFF
     digitalWrite(LED_PIN, LOW);
 
-    // Wait for 5 seconds
-    delay(5000);
+    // Wait for 4 seconds
+    delay(7500);
 }
