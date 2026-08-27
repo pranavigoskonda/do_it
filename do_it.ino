f7ce03995d80e646a6675396bc2e8fd9d8b4716c
pranavigoskonda <pranavi.goskonda.427@my.csun.edu>
2026-08-26 23:44:58 -0700

    add this program

---
+++ do_it.ino
@@ -0,0 +1,22 @@
+// ESP32 Built-in LED Blink Program
+
+const int LED_PIN = 2;
+
+void setup() {
+    // Configure GPIO 2 as an output
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    // Turn the blue LED ON
+    digitalWrite(LED_PIN, HIGH);
+
+    // Wait for 5 seconds
+    delay(5000);
+
+    // Turn the blue LED OFF
+    digitalWrite(LED_PIN, LOW);
+
+    // Wait for 5 seconds
+    delay(5000);
+}
